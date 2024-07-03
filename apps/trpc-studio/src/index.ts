79b405179e5fc86e16a341802997803b216853e4
acidMyke <13977709+acidMyke@users.noreply.github.com>
2024-07-03 18:47:50 +0800

    do a small test with tRPC v11

--- apps/trpc-studio/src/index.ts
+++ apps/trpc-studio/src/index.ts
@@ -1,13 +1,36 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
-import { findAppRouter } from './utils/trpc';
+import { Procedure, findAppRouter } from './utils/trpc';
 import { Config } from './config';
+import { resolve as resovlePath } from 'path';
+import { stat as fsStat } from 'fs/promises';
 
 export async function startStudio(args: Config) {
-  // TODO: Ensure path in args is absolute, and file exists
-  // TODO: Find the app router in the file
-  // TODO: Parse the router, retrieve the procedures and their input schema
+  // Ensure path in args is absolute, and file exists
+  const routerPath = resovlePath(args.routerPath);
+  try {
+    await fsStat(routerPath);
+  } catch (error) {
+    console.error('Invalid router path:', error);
+    process.exit(1);
+  }
+  // Find the app router in the file
+  const appRouter = await findAppRouter(routerPath);
+  const procedureInfos: Map<string, ReturnType<typeof extractInfo>> = new Map();
+  for (const path in appRouter._def.procedures) {
+    const procedure = appRouter._def.procedures[path];
+    procedureInfos.set(path, extractInfo(procedure));
+  }
+
+  console.log(procedureInfos);
   // TODO: Start Hono Server
+}
+
+function extractInfo(procedure: Procedure) {
+  // TODO: Extract zod schemas from procedure
+  // TODO: Extract the schema info from Zod and make it serializable
+  console.log(procedure);
+  return procedure;
 }
 
 // const app = new Hono();
