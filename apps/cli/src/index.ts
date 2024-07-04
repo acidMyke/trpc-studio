f810d1708c90dc2777280d0a6a8a2a5264d162d0
acidMyke <13977709+acidMyke@users.noreply.github.com>
2024-07-05 00:00:42 +0800

    handle procedure execution

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,8 +1,12 @@
-import { extractInfo, findAppRouter } from './utils/trpc';
+import { createClient, extractInfo, findAppRouter } from './utils/trpc';
 import { Config } from './config';
 import consola from 'consola';
 import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
+import { z } from 'zod';
+import { TRPCClientError } from '@trpc/client';
+import json5 from 'json5';
+import { StatusCode } from 'hono/utils/http-status';
 
 export async function startStudio(args: Config) {
   const { routerPath } = args;
@@ -14,9 +18,10 @@ export async function startStudio(args: Config) {
     procedureInfos.set(path, extractInfo(path, procedure));
   }
   consola.info(`Found ${procedureInfos.size} procedures`);
+  // tRPC Client to communicate with the server
+  const trpc = createClient({ url: args.trpcEndpoint });
 
   //Start Hono Server
-
   const hono = new Hono();
 
   const apiPath = hono.basePath('/api');
@@ -25,8 +30,64 @@ export async function startStudio(args: Config) {
     return c.json(Array.from(procedureInfos.keys()));
   });
 
-  apiPath.get('/procedures/:procedure', c => {
-    return c.json(procedureInfos.get(c.req.param('procedure')));
+  apiPath.get('/procedures/:path', c => {
+    return c.json(procedureInfos.get(c.req.param('path')));
+  });
+
+  const executeSchema = z.object({
+    data: z.unknown(),
+    method: z.enum(['query', 'mutation'], {
+      message: 'Invalid method: only query or mutation allowed',
+    }),
+  });
+
+  apiPath.post('/procedures/:path/execute', async c => {
+    const path = c.req.param('path');
+    try {
+      const body = json5.parse(await c.req.text());
+      const input = executeSchema.parse(body);
+      const start = Date.now();
+      const res = await trpc[input.method](path, input.data)
+        .then(res => ({ success: true as const, data: res }))
+        .catch(err => ({ success: false as const, error: err }));
+      const end = Date.now();
+      let status: StatusCode = res.success ? 200 : 502; // Reserve 500 for users API server errors
+      if (!res.success) {
+        if (res.error instanceof TRPCClientError) {
+          if ('data' in res.error && typeof res.error.data === 'object') {
+            status = (res.error.data.httpStatus ?? status) as StatusCode;
+            res.error = {
+              message:
+                res.error.message ??
+                (res.error.meta?.responseJSON as any)?.error?.message ??
+                'Unknown error',
+              ...res.error.data,
+            };
+          }
+        }
+      }
+      return c.json(
+        {
+          response: res,
+          timeTaken: end - start,
+        },
+        status
+      );
+    } catch (err) {
+      if (err instanceof SyntaxError) {
+        // JSON.parse error
+        return c.json(
+          { error: 'Invalid JSON', name: err.name, message: err.message },
+          400
+        );
+      } else if (err instanceof z.ZodError) {
+        // Zod error
+        return c.json({ error: err.errors }, 400);
+      } else {
+        // Unknown error
+        throw err;
+      }
+    }
   });
 
   serve(
