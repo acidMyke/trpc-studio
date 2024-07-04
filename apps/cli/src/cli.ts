5f7e73f78abb5f4c62016a5f890417045b295869
acidMyke <13977709+acidMyke@users.noreply.github.com>
2024-07-04 10:03:51 +0800

    Slight CLI changes

--- apps/cli/src/cli.ts
+++ apps/cli/src/cli.ts
@@ -2,14 +2,23 @@
 // import { main, configSchema } from '.';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import { parseConfigurationFile, configSchema } from './config';
+import {
+  parseConfigurationFile,
+  configSchema,
+  usableConfigFileNames,
+} from './config';
 import { startStudio } from '.';
 import { fromZodError } from 'zod-validation-error';
 import consola from 'consola';
 
 const args = yargs(hideBin(process.argv))
   .usage(
-    `tRPC Studio\nProvides a simple Web UI to test tRPC procedures without implementing a client.`
+    `tRPC Studio
+Provides a simple Web UI to test tRPC procedures without implementing a client.
+
+Configuration can be provided via any of the following files:
+${usableConfigFileNames.join('\n')}
+`
   )
   .option('routerPath', {
     alias: 'p',
@@ -21,19 +30,23 @@ const args = yargs(hideBin(process.argv))
     type: 'string',
     description: 'Endpoint that handle the trpc requests',
   })
-  .example([
-    [
-      '$0 --routerPath ./src/api/root.ts --trpcEndpoint http://localhost:3000/api/trpc',
-      'Start tRPC Studio for T3 Stack',
-    ],
-  ])
-  .usage(
-    `Configuration can also be provided by any of the following file in the root of your project`
-  )
+  .options('verbose', {
+    alias: 'v',
+    type: 'boolean',
+    description: 'Run with verbose logging',
+  })
+  .options('debug', {
+    type: 'boolean',
+    description: 'Run with debug logging',
+  })
+  .options('silent', {
+    type: 'boolean',
+    description: 'Run with no logging',
+  })
   .help()
   .version()
   .alias('help', 'h')
-  .alias('version', 'v').argv;
+  .alias('version', 'V').argv;
 
 parseConfigurationFile().then(configFromFile => {
   // Combine the configuration from the file and the arguments, arguments will override the file
@@ -54,6 +67,8 @@ parseConfigurationFile().then(configFromFile => {
     );
     process.exit(1);
   }
+  consola.level = data.verbose ? +999 : data.debug ? 4 : data.silent ? -999 : 3;
 
+  consola.info('Starting tRPC Studio');
   return startStudio(data);
 });
