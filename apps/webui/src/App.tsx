b40df2d1a78b857abfa6dada5e1807859096c28d
acidMyke <13977709+acidMyke@users.noreply.github.com>
2024-07-06 23:08:59 +0800

    webui: Add procedure selection menu to left side panel

--- apps/webui/src/App.tsx
+++ apps/webui/src/App.tsx
@@ -1,14 +1,57 @@
 import { useState } from 'react';
 import { ThemeProvider } from './providers/theme';
 import ResizeableContainer from './components/ResizeableContainer';
+import { getProcedures } from './api';
+import { useQuery } from '@tanstack/react-query';
+import { twMerge } from 'tailwind-merge';
+
+const procedureUi = {
+  query: ['Q', 'badge-primary'],
+  mutation: ['M', 'badge-secondary'],
+  subscription: ['S', 'badge-error'],
+} as const;
+
+interface ProcedureMenuItemProps {
+  path: string;
+  type: keyof typeof procedureUi;
+}
+
+function ProcedureMenuItem({ path, type }: ProcedureMenuItemProps) {
+  const [icon, color] = procedureUi[type];
+  return (
+    <li key={path} className={type === 'subscription' ? 'disabled' : ''}>
+      <a onClick={e => e.preventDefault()}>
+        <span className={twMerge('badge inline', color)}>{icon}</span>
+        {path}
+      </a>
+    </li>
+  );
+}
 
 function App() {
+  const { data: procedures, isPending: isProcedurePending } = useQuery({
+    queryKey: ['procedures'],
+    queryFn: getProcedures,
+  });
+
   return (
     <div className='fixed inset-0'>
       <ThemeProvider>
-        <ResizeableContainer>
-          <div>Left</div>
-          <div>Right</div>
+        <ResizeableContainer
+          defaultWidth='16rem'
+          maxWidth='24rem'
+          minWidth='8rem'
+        >
+          {isProcedurePending || !procedures ? (
+            <div className='skeleton h-full w-full' />
+          ) : (
+            <ul className='h-full w-full menu bg-base-200 overflow-clip'>
+              {Object.entries(procedures)?.map(([path, type]) => (
+                <ProcedureMenuItem key={path} path={path} type={type} />
+              ))}
+            </ul>
+          )}
+          <div className='flex flex-col h-full'></div>
         </ResizeableContainer>
       </ThemeProvider>
     </div>
